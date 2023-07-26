6c2f8bc58b4193a9aa152a6120f06337ed8150ce
codehyouka <plants.coordinators@gmail.com>
2023-07-26 22:07:21 +0800

    Delete install.sh

--- bin/install.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-#echo $LINE | sed -e "s/12345678/${replace}/g"
-
-#echo "${PWD}/${0}"
-
-cd ../ || gem install
-echo "${0}"
-#ruby /home/peinfs/Desktop/research/explore/codereview_m1/application/app/main.rb
