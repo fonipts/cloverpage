0e54937a8b52bec67da70844fe2c4625757c74fb
codeyouka <plants.coordinators@gmail.com>
2023-07-28 20:26:35 +0800

    Code cleanup

--- app/library/extension/codescan/global/max_newline_limit.rb
+++ app/library/extension/codescan/global/max_newline_limit.rb
@@ -8,8 +8,6 @@ class MaxNewlineLimitInCode < CodeScanInterface
       @ext_log = []
       @ext_details = {}
 
-      @is_error = false
-      @valid_line_code = []
    end
 
    def setData(name, content, config,log)
@@ -22,38 +20,30 @@ class MaxNewlineLimitInCode < CodeScanInterface
 
       count =1
 
-      reference = -1
       newline_count =0
-      @valid_line_code = []
-     
+
       for line in @ext_content.getReadLine
 
          if line.strip == ""
 
             newline_count+=1
-            if newline_count>@ext_config 
-               
-               if reference == -1
-                  reference = count-1
-               end
+            if newline_count>@ext_config
+
                template_msg ="file `%s` found %s or more newline at line code %s"% [@ext_name,@ext_config, count]
 
                @ext_log.append(template_msg)
                @is_error = true
                @ext_content.setDeleteAtReadLine( count -1 )
-            else
-               reference = -1
             end
          else
-            @valid_line_code.append(line)
+
             newline_count = 0
-            reference = -1
+
          end
          count +=1
       end
 
    end
-
 
 
 end
