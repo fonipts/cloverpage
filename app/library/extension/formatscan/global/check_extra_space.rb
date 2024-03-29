612863f6f239f55999b87d02036b4b06842bc25d
codehyouka <plants.coordinators@gmail.com>
2024-03-30 01:08:39 +0800

    Update check_extra_space.rb

--- app/library/extension/formatscan/global/check_extra_space.rb
+++ app/library/extension/formatscan/global/check_extra_space.rb
@@ -14,7 +14,7 @@ class CheckExtraSpaceInCode < CodeScanInterface
     return unless @ext_config
 
     msg = ''
-    reg_a1 = /(\s{0,})([\!]{0,1}={2,}|[\!\<\>]{1}={1})(\s{0,})/
+    reg_a1 = /(\s{0,})([\!]{0,1}={2,}|[\!\<\>]{1}={1}|[\<\>])(\s{0,})/
     reg_a2 = %r{(\s{0,})([/*\-+]={1,})(\s{0,})}
     reg_a21 = /^[\s]{0,}([\/\*-+])/
     count = 1
@@ -26,14 +26,22 @@ class CheckExtraSpaceInCode < CodeScanInterface
       match2 = msg.match(reg_a2)
       match21 = msg.match(reg_a21)
 
-      if match1 && (match1[1] != ' ' || match1[3] != ' ')
-        @ext_content.modify_read_line(count - 1, msg.gsub(reg_a1, ' ' + match1[2] + ' '))
-        template_msg = format('file has ` == ` has no equal spacing %<count>s', count: count)
+      if match1 && (match1[1] != ' ' || match1[3] != ' ') && !match21
+        #@ext_content.modify_read_line(count - 1, msg.gsub(reg_a1, ' ' + match1[2] + ' '))
+        str_rep = line.to_s.gsub!(reg_a1) do |m|
+          
+          ' '+ Regexp.last_match(2)+' '
+        end
+        @ext_content.modify_read_line(count - 1, str_rep)
+        template_msg = format('file has `%<sign>s` has no equal spacing %<count>s', count: count,sign: match1[2])
         @ext_log.append(template_msg)
       end
       if match2 && (match2[1] != ' ' || match2[3] != ' ') && !match21
-       
-        @ext_content.modify_read_line(count - 1, msg.gsub(reg_a2, ' ' + match2[2] + ' '))
+        str_rep = line.to_s.gsub!(reg_a2) do |m|
+          
+          ' '+ Regexp.last_match(2)+' '
+        end
+        @ext_content.modify_read_line(count - 1, str_rep)
         template_msg = format('file has ` %<sign>s ` has no equal spacing %<count>s', count: count, sign: match2[2])
         @ext_log.append(template_msg)
       end
