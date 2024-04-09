da440e56cd2531a360a24c04581c888cc4f51f64
pein <plants.coordinators@gmail.com>
2024-04-10 00:22:00 +0800

    Remove unused variable

--- app/config/app.rb
+++ app/config/app.rb
@@ -4,11 +4,6 @@ module AppDefaultVaribles
   @default_extname = 'yaml'
   @default_filename_with_extname = '%s.%s'
 
-  @type_codescan = 'codescan'
-  @type_httprequest = 'httprequest'
-  @type_formatscan = 'formatscan'
-  @valid_script_action = [@type_codescan, @type_apirequest, @type_formatscan]
-
   def self.default_filename
     @default_filename
   end
@@ -19,22 +14,6 @@ module AppDefaultVaribles
 
   def self.default_filename_with_extname
     format(@default_filename_with_extname, @default_filename, @default_extname)
-  end
-
-  def self.type_codescan
-    @type_codescan
-  end
-
-  def self.type_httprequest
-    @type_httprequest
-  end
-
-  def self.type_formatscan
-    @type_formatscan
-  end
-
-  def self.valid_script_action
-    @valid_script_action
   end
 
   def self.default_version
